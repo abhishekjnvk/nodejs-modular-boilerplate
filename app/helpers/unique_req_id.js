ce9061fb9b00aeb54b19a55d1f526261673b9d1e
Abhishek Kumar <abhishekkumarjnvk@gmail.com>
2023-08-24 00:40:42 +0530

    upgrade mongoose

--- app/helpers/unique_req_id.js
+++ app/helpers/unique_req_id.js
@@ -1,8 +1,8 @@
-const cuid = require('cuid');
+const { nanoid } = require('nanoid');
 const httpContext = require('express-http-context');
 
 const uniqueReqId = (req, res, next) => {
-  const reqId = cuid();
+  const reqId = nanoid();
   httpContext.set('req_id', reqId);
   next();
 };
